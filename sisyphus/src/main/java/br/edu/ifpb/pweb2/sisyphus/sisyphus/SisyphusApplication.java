6fa79ae15ad477b6c6a0ca410117c3536f65ce93
Luiz Gonzaga <luizg.academico@gmail.com>
2023-08-26 15:36:36 -0300

    Criação do Projeto em Spring Boot

---
+++ sisyphus/src/main/java/br/edu/ifpb/pweb2/sisyphus/sisyphus/SisyphusApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ifpb.pweb2.sisyphus.sisyphus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SisyphusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SisyphusApplication.class, args);
+	}
+
+}
