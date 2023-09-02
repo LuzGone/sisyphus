9cf0a5eaeefa23a00eb256a3fc02d440a88f21c0
Louise <louisefcaetano@gmail.com>
2023-09-02 14:06:06 -0300

    Classes Aluno e Assunto

--- sisyphus/src/main/java/br/edu/ifpb/pweb2/sisyphus/model/Aluno.java
+++ sisyphus/src/main/java/br/edu/ifpb/pweb2/sisyphus/model/Aluno.java
@@ -1,5 +1,66 @@
 package br.edu.ifpb.pweb2.sisyphus.model;
 
 public class Aluno {
-    
+    private int id;
+    private String nome;
+    private String fone;
+    private String matricula;
+    private String login;
+    private String senha;
+
+    public Aluno(String nome, String fone, String matricula, String login, String senha) {
+        this.nome = nome;
+        this.fone = fone;
+        this.matricula = matricula;
+        this.login = login;
+        this.senha = senha;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getFone() {
+        return fone;
+    }
+
+    public void setFone(String fone) {
+        this.fone = fone;
+    }
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
 }
--- sisyphus/src/main/java/br/edu/ifpb/pweb2/sisyphus/model/Assunto.java
+++ sisyphus/src/main/java/br/edu/ifpb/pweb2/sisyphus/model/Assunto.java
@@ -1,5 +1,26 @@
 package br.edu.ifpb.pweb2.sisyphus.model;
 
 public class Assunto {
-    
+    private int id;
+    private String nome;
+
+    public Assunto(String nome) {
+        this.nome = nome;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
 }
