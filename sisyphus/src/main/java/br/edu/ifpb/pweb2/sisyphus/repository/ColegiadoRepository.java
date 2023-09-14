afff888b7485358b360e634f0acebbfff9171a5b
Louise <louisefcaetano@gmail.com>
2023-09-14 16:26:06 -0300

    repositorios

---
+++ sisyphus/src/main/java/br/edu/ifpb/pweb2/sisyphus/repository/ColegiadoRepository.java
@@ -0,0 +1,8 @@
+package br.edu.ifpb.pweb2.sisyphus.repository;
+
+import br.edu.ifpb.pweb2.sisyphus.model.Colegiado;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ColegiadoRepository extends JpaRepository<Colegiado, Integer> {
+    
+}
