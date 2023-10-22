a02590c6d286258b67ae89d7d8a6283c3b70304f
Louise <louisefcaetano@gmail.com>
2023-10-21 21:49:50 -0300

    alteração classe Professor, criação repositorio e professor-form

--- sisyphus/src/main/java/br/edu/ifpb/pweb2/sisyphus/model/Professor.java
+++ sisyphus/src/main/java/br/edu/ifpb/pweb2/sisyphus/model/Professor.java
@@ -4,7 +4,11 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@NoArgsConstructor
 @Entity
 public class Professor {
     @Id
@@ -28,59 +32,8 @@ public class Professor {
         this.coordenador = coordenador;
     }
     
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getFone() {
-        return fone;
-    }
-
-    public void setFone(String fone) {
-        this.fone = fone;
-    }
-
-    public String getMatricula() {
-        return matricula;
-    }
-
-    public void setMatricula(String matricula) {
-        this.matricula = matricula;
-    }
-
-    public String getLogin() {
-        return login;
-    }
-
-    public void setLogin(String login) {
-        this.login = login;
-    }
-
-    public String getSenha() {
-        return senha;
-    }
-
-    public void setSenha(String senha) {
-        this.senha = senha;
-    }
-
     public boolean isCoordenador() {
         return coordenador;
     }
 
-    public void setCoordenador(boolean coordenador) {
-        this.coordenador = coordenador;
-    }
 }
---
+++ sisyphus/src/main/java/br/edu/ifpb/pweb2/sisyphus/repository/ProfessorRepository.java
@@ -0,0 +1,10 @@
+package br.edu.ifpb.pweb2.sisyphus.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import br.edu.ifpb.pweb2.sisyphus.model.Professor;
+
+@Repository
+public interface ProfessorRepository extends JpaRepository<Professor,Long> {
+    
+}
