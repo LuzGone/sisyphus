c1c785832a43630815ca092a9e52971aef3c1f42
Luiz Gonzaga <luizg.academico@gmail.com>
2023-12-20 20:10:03 -0300

    Interceptadores não estão funcionando corretamente

--- sisyphus/src/main/java/br/edu/ifpb/pweb2/sisyphus/controller/AutenticadorController.java
+++ sisyphus/src/main/java/br/edu/ifpb/pweb2/sisyphus/controller/AutenticadorController.java
@@ -17,7 +17,6 @@ import jakarta.servlet.http.HttpSession;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
