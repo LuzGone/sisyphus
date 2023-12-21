9f0be5b1c277fe1070f74f4847a15b5b5209f2f3
jardsousa <aaaedenn47@gmail.com>
2023-12-21 00:55:26 -0300

    tentativa de implementação do deu a louca no fredinho (validação)

---
+++ sisyphus/src/main/java/br/edu/ifpb/pweb2/sisyphus/validator/OnlyNumberMatConstraintValidador.java
@@ -0,0 +1,21 @@
+
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+
+public class OnlyNumberMatConstraintValidador implements ConstraintValidator<OnlyNumberMat, String> {
+
+    @Override
+    public void initialize(OnlyNumberMat constraintAnnotation) {
+        // Configuração inicial, se necessário
+    }
+
+    @Override
+    public boolean isValid(String value, ConstraintValidatorContext context) {
+        // Lógica de validação: verificar se a matrícula contém apenas números
+        if (value == null) {
+            return true; // Aceitar valores nulos, se aplicável
+        }
+
+        return value.matches("\\d+"); // Verificar se contém apenas números
+    }
+}
